b38686494083f817f03031fb8b7d747599e470df
GordonChen15 <chinglunchenccl@gmail.com>
2024-09-08 12:44:00 +0100

    Q6.py

--- Q6.py
+++ Q6.py
@@ -6,3 +6,6 @@ def high_value(my_array):
         if my_array[i] > highest:
             highest = my_array[i]
     print(highest)
+
+# The last line should be changed into "return(highest)" to make this code a function.
+# "print(high_value(my_array))" can output the result.
