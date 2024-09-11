765f263ae1090d105a941b4aa778e52f3b4b81ea
Gordon Chen <chinglunchenccl@gmail.com>
2024-09-11 11:41:09 +0100

    Delete Q1.py

--- Q1.py
+++
@@ -1,4 +0,0 @@
-#Write a program which prompts the user for a string and outputs the same string but with the first and last character exchanged.
-a = str(input("Type a string"))
-#a[0], a[len(a)-1] = a[len(a)-1], a[0] (but chr in strings cannot be assigned)
-print(a[len(a)-1]+a[1:len(a)-1]+a[0])
