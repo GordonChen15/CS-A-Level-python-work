89a9027db84237e5343fd6768b36c04f7a69d92e
Gordon Chen <chinglunchenccl@gmail.com>
2024-09-11 12:03:30 +0100

    Update 11.2 Prep.py Q4-5

--- 11.2 Prep.py
+++ 11.2 Prep.py
@@ -1,24 +1,24 @@
 #Q4
-print(“Select one of the following options: ”)
-print(“Enter A for Multiply: ”)
-print(“Enter B for Divide: ”)
-print(“Enter C for Add: ”)
-print(“Enter D for Subtract: ”)
-print(“Enter E for Remainder Division: ”)
-num1 = int(input(“Enter the first number”))
-num2 = int(input(“Enter the second number”))
-operation = str(input(“Enter the letter for your chosen operation.”))
-match operation
-case “A”:
-  print(num1*num2)
-case “B”:
-	print(num1/num2)
-case"C":
-	print(num1+num2)	
-case"D":
-	print(num1-num2)
-case"E”:
-	print(num1%num2)
+print("Select one of the following options: ")
+print("Enter A for Multiply:")
+print("Enter B for Divide: ")
+print("Enter C for Add: ")
+print("Enter D for Subtract: ")
+print("Enter E for Remainder Division: ")
+num1 = int(input("Enter the first number"))
+num2 = int(input("Enter the second number"))
+operation = str(input("Enter the letter for your chosen operation."))
+match operation:
+    case "A":
+        print(num1*num2)
+    case "B":
+	    print(num1/num2)
+    case "C":
+        print(num1+num2)	
+    case"D":
+	    print(num1-num2)
+    case"E":
+	    print(num1%num2)
 
 #Q5
 Year = int(input("Please input a year"))
