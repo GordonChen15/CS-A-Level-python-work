8b66bb7e62b1409350c8c88e1a319b06f97cfe19
Gordon Chen <chinglunchenccl@gmail.com>
2024-09-11 11:42:39 +0100

    Delete Q5.py

--- Q5.py
+++
@@ -1,8 +0,0 @@
-# The average of non-negative numbers entered.
-count, total = 0, 0
-a = int(input("Enter a number: "))
-while a >= 0:
-    count += 1
-    total += a
-    a = int(input("Enter a number: "))
-print(total, count)
